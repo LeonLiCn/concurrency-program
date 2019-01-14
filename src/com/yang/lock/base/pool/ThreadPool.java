4b9fd8c3c2a8724c56872ce71b82880f974f0d6d
yang <leon.li.three@gmail.com>
2019-01-14 21:24:32 +0800

    8 character code

--- src/com/yang/lock/base/pool/ThreadPool.java
+++ src/com/yang/lock/base/pool/ThreadPool.java
@@ -7,7 +7,26 @@ public class ThreadPool {
     private static ThreadPoolExecutor tr = new ThreadPoolExecutor(5, 15, 100000,
             TimeUnit.SECONDS, new LinkedBlockingQueue<>());
 
+    private static ExecutorService es = Executors.newFixedThreadPool(5);
+    private static ExecutorService es1 = Executors.newSingleThreadExecutor();
+    private static ExecutorService es2 = Executors.newCachedThreadPool();
+    private static ExecutorService es3 = Executors.newScheduledThreadPool(5);
+
     public static void main(String[] args) {
+
+        FutureTask<String> ft = new FutureTask<>(() -> {
+            return "hello";
+        });
+        ft.run();
+        tr.execute(ft);
+        try {
+            ft.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
         Future<String> f = tr.submit(() -> {
             return "hello world";
         });
