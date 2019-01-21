4bb0d4f76f810cb06e0163379807f9715d775573
liyang <ly@qtbigdata.com>
2019-01-21 19:51:14 +0800

    jvm note

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
 
     public static void main(String[] args) {
         Map<String, Object> map = new ConcurrentHashMap<>();
+        map.get(null);
+        Map<String, Object> map1 = new HashMap<>();
+        ReentrantLock lock = new ReentrantLock();
+        lock.lock();
+        String s = "ss";
         System.out.println("Hello World!");
     }
 }
